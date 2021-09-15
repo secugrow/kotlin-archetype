c6707264690be77faa394190a173fffdbf0776a8
Boris Wrubel <boris.wrubel@gmx.at>
2021-09-15 12:20:07 +0200

    Updated Factory for Android Appium Web (chrome)

--- src/main/resources/archetype-resources/src/test/kotlin/driverutil/AppiumAndroidWebDriverFactory.kt
+++ src/main/resources/archetype-resources/src/test/kotlin/driverutil/AppiumAndroidWebDriverFactory.kt
@@ -23,13 +23,13 @@ class AppiumAndroidWebDriverFactory : RemoteWebDriverFactory() {
 
 
         caps.setCapability("chromedriverExecutableDir", path2chromeDriver)
-        caps.setCapability(MobileCapabilityType.APPIUM_VERSION, "1.13")
+        caps.setCapability(MobileCapabilityType.APPIUM_VERSION, "1.21.0")
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android")
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Appium_Android_Device")
-        //caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator1")
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2")
         caps.setCapability("browserName", "chrome")
         caps.setCapability(MobileCapabilityType.UDID, getMobileDeviceId())
-
+        caps.setCapability("appium:chromeOptions", mutableMapOf(Pair("w3c", false)));
         caps.setCapability("noReset", true)
 
         val appiumServer = URL("$dollar$curlyOpen getRemoteTestingServer$bracketOpen$bracketClose$curlyClose/wd/hub")
