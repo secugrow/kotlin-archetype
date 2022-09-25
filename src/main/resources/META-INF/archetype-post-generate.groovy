3d9257fd183b0e621290ffeecf4ff47474d5fa1d
Adi Musilek <adolf.musilek@wrwks.at>
2022-09-25 18:42:08 +0200

    started implementing post-generate script

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,3 +1,5 @@
+import org.apache.commons.io.FileUtils
+
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -13,11 +15,24 @@ Files.move(
 Properties properties = request.properties
 println " -> properties: $properties"
 
-def packageName = properties.get("package")
+def packageName = properties.get("package").toString()
 println " -> package: $packageName"
 
-def a11y = properties.get("a11y")
+def a11y = properties.get("a11y").toString()
 println " -> a11y: $a11y"
 
-def packagePath = packageName.replace(".", "/")
-println " -> packagePath: $packagePath"
+def packagePath = packageName.replace(".", "/").toString()
+println " -> converted to packagePath: $packagePath"
+
+def packageBasePath = Paths.get(projectPath.toString(), "/src/test/kotlin/", packagePath)
+
+if (Boolean.valueOf(a11y)) {
+    def pathToFile = Paths.get(packageBasePath.toString(), "/step_definitions/AbstractStepDefs_noa11y.kt")
+    println " -> deleting file: $pathToFile"
+    def exists = Files.deleteIfExists(pathToFile.toAbsolutePath())
+    println " -> file was deleted $exists"
+} else {
+    println " -> a11y-value $a11y\n" +
+            "\t -> TODO delete AbstractStepDefs.kt in $packagePath/step_definitions and rename AbstractStepDefs_noa11y.kt to AbstractStepDefs.kt"
+
+}
---
+++ src/main/resources/archetype-resources/src/test/kotlin/step_definitions/AbstractStepDefs_noa11y.kt
@@ -0,0 +1,82 @@
+package ${package}.step_definitions
+
+import assertk.fail
+import ${package}.driverutil.PageNotFoundException
+import ${package}.driverutil.WebDriverSession
+import ${package}.driverutil.WebDriverSessionStore
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.remote.RemoteWebDriver
+import ${package}.pageobjects.AbstractPage
+import io.cucumber.java8.En
+import kotlin.reflect.KClass
+import logger
+
+open class AbstractStepDefs(protected val testDataContainer: TestDataContainer) : En {
+
+    private val log by logger()
+    private var currentSessionID: String = "not set"
+
+    fun getWebDriverSession(): WebDriverSession {
+        if (currentSessionID == "not set") {
+            testDataContainer.setTestData("session.id", testDataContainer.getTestId())
+            currentSessionID = testDataContainer.getTestId()
+        }
+        return getWebDriverSession(currentSessionID)
+    }
+
+    fun getWebDriverSession(sessionID: String): WebDriverSession {
+
+        val webDriverSession = WebDriverSessionStore.get(sessionID)
+
+        if (testDataContainer.needsInitializing()) {
+            if (webDriverSession.webDriver is RemoteWebDriver) {
+                testDataContainer.setTestData("browser.version", (webDriverSession.webDriver as RemoteWebDriver).capabilities.browserVersion)
+                testDataContainer.setTestData("browser", (webDriverSession.webDriver as RemoteWebDriver).capabilities.browserName)
+            }
+
+            if (webDriverSession.webDriver is ChromeDriver) {
+                testDataContainer.setTestData("mobileEmulation", System.getProperty("browser").contains("emulation"))
+            }
+
+            testDataContainer.setTestData("initialized", true)
+        }
+        currentSessionID = sessionID
+        testDataContainer.setTestData("session.id", currentSessionID)
+        return webDriverSession
+    }
+
+    fun getWebDriver(): WebDriver {
+        return getWebDriverSession().webDriver
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T : AbstractPage> getPage(pageClass: KClass<T>): T {
+        val page = getWebDriverSession().currentPage
+
+        if (pageClass.isInstance(page)) {
+            return page as T
+        }
+        log.error("Expect Page from type $pageClass but was $page")
+        throw PageNotFoundException("Expect Page from type $pageClass but was $page")
+    }
+
+    fun getCurrentPage(): AbstractPage? {
+        return getWebDriverSession().currentPage
+    }
+
+    fun setCurrentPage(page: AbstractPage) {
+        getWebDriverSession().currentPage = page
+    }
+
+}
+
+fun extractTestIdFromScenarioName(scenarioName: String): String {
+    val regex = "^\\[(.*) \\[.*\$".toRegex()
+    try {
+        return regex.find(scenarioName)!!.groups[1]!!.value
+    } catch (e: NullPointerException) {
+        fail("Scenarioname is not correct formated $scenarioName. Pattern: '[XXX-99 [Filename]")
+    }
+}
+
