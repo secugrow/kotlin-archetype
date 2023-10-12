b4a101f86c22d6ddddac854abb730eadd2be6de1
adi <adimusilek@proton.me>
2023-10-12 19:30:57 +0200

    a little thread sleep, just for the demo

--- src/main/resources/archetype-resources/src/test/kotlin/pageobjects/WikipediaPage.kt
+++ src/main/resources/archetype-resources/src/test/kotlin/pageobjects/WikipediaPage.kt
@@ -1,5 +1,6 @@
 package ${package}.pageobjects
 
+import java.time.Duration
 import ${package}.driverutil.WebDriverSession
 import ${package}.pageobjects.AbstractPage
 import ${package}.pageobjects.MainPage
@@ -8,7 +9,7 @@ import org.openqa.selenium.By
 class WikipediaPage(session: WebDriverSession) : MainPage(session)  {
 
     fun isSearchbarPresent(): Boolean {
-        return webDriver.findElements(By.id("searchInput")).size >= 1
+        return (webDriver.findElements(By.id("searchInput")).size >= 1).also { Thread.sleep(Duration.ofSeconds(2.toLong())) }
     }
 
 
