6a7278b9d3963fdccec6c7257effba7b5158fe3b
Boris Wrubel <boris@secugrow.io>
2025-04-03 08:09:15 +0200

    Adapted firefox runconfig for Linux (needs additional environmentparameter if firefox is installed via snap)

--- src/main/resources/archetype-resources/src/test/kotlin/driverutil/FirefoxWebDriverFactory.kt
+++ src/main/resources/archetype-resources/src/test/kotlin/driverutil/FirefoxWebDriverFactory.kt
@@ -11,6 +11,12 @@ class FirefoxWebDriverFactory : WebDriverFactory() {
 
         val ffOptions = FirefoxOptions()
 
+        val linux = System.getProperty("os.name").lowercase().contains("linux")
+        if (linux && System.getenv().get("TMPDIR")!!.contains("could_be_necessary_for_firefox_linux")) {
+            log.warn("If you running on Linux and using a snap installed firefox you have to set the TMPDIR to a writeable directory in your homedirectory with the absolute path!")
+        }
+
+
         webDriver = FirefoxDriver(ffOptions.merge(caps))
         webDriver.manage().window().size = screenDimension.dimension
 
