b6d3ed87435e235463468246ceccf8a33c946e56
Boris Wrubel <boris@secugrow.io>
2025-08-26 21:48:37 +0200

    formatting and fixing dependecy for slf4j

--- src/main/resources/archetype-resources/src/test/kotlin/Logger.kt
+++ src/main/resources/archetype-resources/src/test/kotlin/Logger.kt
@@ -1,6 +1,6 @@
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-fun <R : Any> R.logger(): Lazy<Logger> = lazy { LoggerFactory.getLogger(getClassName(this::class.java)) }
+fun <R : Any> R.logger(): Lazy<Logger> = lazy { LoggerFactory.getLogger(this::class.java.name) }
 
-private fun <T : Any> getClassName(clazz: Class<T>): String = clazz.name.replace("""\$.*$""", "")
+private fun <T : Any> getClassName(clazz: Class<T>): String = clazz.name.replace("""\$.*$""".toRegex(), "")
